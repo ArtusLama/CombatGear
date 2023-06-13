9960472f5cf2bc5f6c6ddc26dd45d8148495a1b3
Artus <artuslama@gmail.com>
2023-06-13 14:02:34 +0200

    Initial commit

---
+++ src/main/java/de/artus/gunsystem/GunSystem.java
@@ -0,0 +1,17 @@
+package de.artus.gunsystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GunSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
